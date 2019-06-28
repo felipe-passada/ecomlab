b3c01773b21e5c4f6b40468690e97f450d2b0628
felipe <felipepassada@gmail.com>
2019-06-28 12:14:09 -0300

    user data from db ready

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        foreach (range(1,100) as $index) {
+            $name = $faker->name;
+
+            DB::table('users')->insert([
+                'name' => $name,
+                'email' => Str::snake($name).'@email.com',
+                'profile' => $faker->randomElement(
+                    ['reseller','store']
+                ),
+                'status' => $faker->randomElement(
+                    ['active','inactive']
+                ),
+                'password' => bcrypt('password'),
+    
+            ]);
+        }
+    }
+}
